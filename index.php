862466a72af07f22a6f6c8cff7607f8eba708328
Delaminor <spef18@gmail.com>
2026-02-04 12:58:37 +0300

    Implement integer division from user input
    Add a PHP script to divide two integers from input

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$input = trim(stream_get_contents(STDIN));
+$parts = preg_split('/\s+/', $input);
+
+if (count($parts) < 2) {
+    fwrite(STDERR, "Введите, пожалуйста, число\n");
+    exit(1);
+}
+
+$aRaw = $parts[0];
+$bRaw = $parts[1];
+
+$a = filter_var($aRaw, FILTER_VALIDATE_INT);
+$b = filter_var($bRaw, FILTER_VALIDATE_INT);
+
+if ($a === false || $b === false) {
+    fwrite(STDERR, "Введите, пожалуйста, число\n");
+    exit(1);
+}
+
+if ((int)$b === 0) {
+    fwrite(STDERR, "Делить на 0 нельзя\n");
+    exit(1);
+}
+
+echo ($a / $b) . PHP_EOL;
